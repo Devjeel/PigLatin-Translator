a8a6755d017eb3cf503d542d0b77bcc980648bf7
Devjeel <jeelhp02@gmail.com>
2018-10-22 07:23:56 -0400

    Add project files.

---
+++ 200395854A2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace _200395854A2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLatin = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Location = new System.Drawing.Point(29, 41);
+            this.txtEnglish.Multiline = true;
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEnglish.Size = new System.Drawing.Size(594, 117);
+            this.txtEnglish.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter English Text Here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pig Latin Translation:";
+            // 
+            // txtLatin
+            // 
+            this.txtLatin.Location = new System.Drawing.Point(29, 234);
+            this.txtLatin.Multiline = true;
+            this.txtLatin.Name = "txtLatin";
+            this.txtLatin.ReadOnly = true;
+            this.txtLatin.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLatin.Size = new System.Drawing.Size(594, 117);
+            this.txtLatin.TabIndex = 3;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(29, 382);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(127, 41);
+            this.btnTranslate.TabIndex = 4;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(203, 382);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 41);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(500, 382);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(123, 41);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 444);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtLatin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnglish);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLatin;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 200395854A2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _200395854A2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Operation
+            //The user enters text in the first multi-line text box and clicks the Translate button or presses the Enter key.
+            //The application translates the text to Pig Latin and displays it in the second multi-line text box.
+            //To clear both text boxes, the user clicks the Clear button, or hits the escape button.
+
+        //Specifications
+            //(Done)If a word starts with a vowel, just add way to the end of the word.
+            //(Done)If a word starts with a consonant, move the consonants before the first vowel to the end of the word and add ay.
+            //If a word starts with the letter Y, the Y should be treated as a consonant. If the Y appears anywhere else in the word, it should be treated as a vowel.
+            //Keep the case of the original word whether it’s uppercase (TEST), title case (Test), or lowercase (test).
+            //Keep all punctuation at the end of the translated word.
+            //(Done)Translate words with contractions. For example, can’t should be an’tcay.
+            //(Works 99%)Don’t translate words that contain numbers or symbols. For example, 123 should be left as 123, and bill@microsoft.com should be left as bill @microsoft.com.
+            //Check that the user has entered text before performing the translation.
+            //Solution & Project should be named StudentNumberA2 (IE 123123123A2)
+            //The unit test project should be named StudentNumberA2_Tests(IE 123123123A2_Tests)
+            //Submit a zip file named StudentNumberA2.zip(ie. 123123123A2.zip) which extracts a folder named StudentNumberA2(ie 123123123A2) which contains your sln file, and project folder with its applicable files inside.
+            //Code must be Unit tested, and the unit tests appropriately documented
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            String EnglishText = txtEnglish.Text;
+            String PigLatin = txtLatin.Text = "";
+            const String vowels = "AEIOUaeiou";
+
+            foreach (string word in EnglishText.Split(' '))
+            {
+                String firstLetter = word.Substring(0, 1);
+                String restOfWord = word.Substring(1, word.Length - 1);
+                int currentLetter = vowels.IndexOf(firstLetter);
+
+                //If there is number or special Chars. , do not translate 
+                if(isNumChar(word))
+                {
+                    PigLatin = word + " ";
+                }
+
+                //Strats with no vowel, i.e. consonant
+                else if (currentLetter == -1)
+                {
+                    PigLatin += restOfWord + firstLetter + "ay ";
+                }
+
+                //There is vowel in firstLetter, i.e. Index will not be -1
+                else
+                {
+                    PigLatin += word + "way ";
+                }
+            }
+
+            txtLatin.Text = PigLatin;
+        }
+
+        public static bool isNumChar(string word)
+        {
+            const String numbersAndSpecialChar = "0123456789!@#$%^&*()";
+            bool value = false;
+
+            for (var i=0; i<word.Length; i++)
+            {
+                int isNumChar = numbersAndSpecialChar.IndexOf(word[i]);
+                if (isNumChar != -1)
+                {
+                    value =  true;
+                }
+
+                i++;
+            }
+            return value;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtEnglish.Clear();
+            txtLatin.Clear();
+        }
+    }
+}
