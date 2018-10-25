55f3f41a7629cc82405c26c753a080de4e831f7e
Devjeel <jeelhp02@gmail.com>
2018-10-25 19:18:31 -0400

    Added Commenting

--- 200395854A2/Form1.cs
+++ 200395854A2/Form1.cs
@@ -24,7 +24,7 @@ namespace _200395854A2
         //Specifications
             //(Done)If a word starts with a vowel, just add way to the end of the word.
             //(Done)If a word starts with a consonant, move the consonants before the first vowel to the end of the word and add ay.
-            //(50%)If a word starts with the letter Y, the Y should be treated as a consonant. If the Y appears anywhere else in the word, it should be treated as a vowel.
+            //(Done)If a word starts with the letter Y, the Y should be treated as a consonant. If the Y appears anywhere else in the word, it should be treated as a vowel.
             //(Done)Keep the case of the original word whether it’s uppercase (TEST), title case (Test), or lowercase (test).
             //(Done)Keep all punctuation at the end of the translated word.
             //(Done)Translate words with contractions. For example, can’t should be an’tcay.
@@ -34,20 +34,24 @@ namespace _200395854A2
             //The unit test project should be named StudentNumberA2_Tests(IE 123123123A2_Tests)
             //Submit a zip file named StudentNumberA2.zip(ie. 123123123A2.zip) which extracts a folder named StudentNumberA2(ie 123123123A2) which contains your sln file, and project folder with its applicable files inside.
             //Code must be Unit tested, and the unit tests appropriately documented
+
         private void btnTranslate_Click(object sender, EventArgs e)
         {
             String EnglishText = txtEnglish.Text;
 
+            //If textbox is empty
             if (String.IsNullOrEmpty(EnglishText))
             {
                 MessageBox.Show("Please enter something you want to tanslate !!!");
                 return;
             }
 
+            //Declaring translated string, reset every time
             String PigLatin = txtLatin.Text = "";
 
             foreach (string word in EnglishText.Split(' '))
             {
+                //Call toPigs method and add word one by one
                 PigLatin += toPigs(word);
             }
 
@@ -56,11 +60,18 @@ namespace _200395854A2
 
         public static String toPigs(string word)
         {
+            //Vowels String
             const String vowels = "AEIOUaeiou";
 
             String PigLatin = "";
+
+            //taking first letter from word
             String firstLetter = word.Substring(0, 1);
+
+            //Restof letters from word
             String restOfWord = word.Substring(1, word.Length - 1);
+
+            //firstletter's index from vowel string
             int currentLetter = vowels.IndexOf(firstLetter);
 
             //If there is number or special Chars. , do not translate 
@@ -98,14 +109,19 @@ namespace _200395854A2
          
         public static bool isUpperCase(string word)
         {
+            //Create an Array, size of word length
             int[] values = new int[word.Length];
+
+            //Add all letter from word by spliting it in 1 letter
             String[] letter = word.Split();
-            bool isCaps = true;
+            bool isCaps = true; //Default value
 
             for (int i=0; i<word.Length; i++)
             {
+                //Add value to Array
                 values[i] = (int)word[i];
 
+                //if ASCII value is between 65 to 90, it is Caps
                 if (values[i] >= 65 && values[i] <= 90)
                     isCaps = true;
                 else
@@ -119,16 +135,20 @@ namespace _200395854A2
         {
             const string numbersAndSpecialChar = "0123456789!@#$%^&*()";
             bool value = false;
-            //string[] letter = word.Split();
 
+            //Loop every letter from word 
             for(int i=0; i<word.Length; i++)
             {
                 int isNumChar = numbersAndSpecialChar.IndexOf(word[i]);
+
+                //Index will not be -1, if there is number or Special Char
                 if (isNumChar != -1)
                 {
                     value = true;
                 }
             }
+
+            //return the boolean value 
             return value;
         }
 
@@ -136,12 +156,14 @@ namespace _200395854A2
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            //Close Application
             this.Close();
             Application.Exit();
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //Clear Textboxes and focus back on english textbox
             txtEnglish.Clear();
             txtLatin.Clear();
             txtEnglish.Focus();
